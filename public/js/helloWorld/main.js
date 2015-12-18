1f58aa266549d10fd3956e129a238316b04a9a6f
Komal <kjain@equalexperts.com>
2015-12-18 10:33:36 +0530

    Made changes in popup example .

---
+++ public/js/helloWorld/main.js
@@ -0,0 +1,1 @@
+ReactDOM.render(<HelloWorld/>, document.getElementById("app"));
--- public/js/popup/test-popup.jsx
+++ public/js/popup/test-popup.jsx
@@ -3,23 +3,31 @@ import React from 'react';
 var TestPopup  = React.createClass({
     getInitialState: function() {
         return {
-            buttonClicked: "None"
+            status: "",
+            showPopup: false
         }
     },
    
    onYes: function() {
-      this.setState({buttonClicked: "Yes"});
+      this.setState({status: "Yes", showPopup: false});
    },
    
    onNo: function() {
-       this.setState({buttonClicked: "No"})
+       this.setState({status: "No", showPopup: false})
+   },
+   
+   displayPopup: function() {
+       this.setState({showPopup: true})
    },
    
    render: function() {
    		return (
                <div>
-                  <h3>Button Clicked:  {this.state.buttonClicked}</h3>
-		          <Popup onYes={this.onYes} onNo={this.onNo}/>
+                  <div className="full-content">
+                    <h3>Deleted:  {this.state.status}</h3>
+                    <button onClick={this.displayPopup}>Delete</button>
+                  </div>
+		          <Popup  display={this.state.showPopup} onYes={this.onYes} onNo={this.onNo}/>
                 </div>
 		   );
    }
@@ -28,9 +36,12 @@ var TestPopup  = React.createClass({
 var Popup = React.createClass({
     render: function(){
         return (
-                <div>
-                    <button type="button" onClick={this.props.onYes}>Yes</button>
-                    <button type="button" onClick={this.props.onNo}>No</button>
+                <div className="popup" style={{display: this.props.display? "block": "none"}}>
+                    <div className="popup-content">
+                        <div>Are you sure?</div>
+                        <button type="button" onClick={this.props.onYes}>Yes</button>
+                        <button type="button" onClick={this.props.onNo}>No</button>
+                    </div>
                 </div>    
             );
     }
