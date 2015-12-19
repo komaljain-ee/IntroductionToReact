44766001527221b60e0e6c2c3e950242c0906d71
Komal <kjain@equalexperts.com>
2015-12-19 12:57:11 +0530

    Create dashboard

---
+++ public/js/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Dashboard = React.createClass({
+	render: function() {
+		return(
+			<div>
+			<button>Create Board</button>
+			</div>
+		)
+	}
+});
+
+module.exports = Dashboard;
---
+++ public/test/dashboard-spec.jsx
@@ -0,0 +1,19 @@
+jest.dontMock("../js/dashboard.jsx");
+
+var React = require('react')
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+
+var Dashboard = require('../js/dashboard.jsx');
+
+describe('Dashboard', () =>{
+	it('should be able to create a Board', () => {
+		var dashboard = TestUtils.renderIntoDocument(
+			<Dashboard/>
+		);
+		
+		var createBoardBtn = ReactDOM.findDOMNode(dashboard, 'button');
+		
+		expect(createBoardBtn.textContent).toBe('Create Board');
+	})
+});
